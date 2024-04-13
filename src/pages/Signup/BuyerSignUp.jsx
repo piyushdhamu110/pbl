b7b855df0e1ec4cc9e0a6e526b0948c8e7c33053
darkars33 <darshangarad18@gmail.com>
2024-04-13 15:15:16 +0530

    update in ui

--- src/pages/Signup/BuyerSignUp.jsx
+++ src/pages/Signup/BuyerSignUp.jsx
@@ -1,11 +1,169 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import img from "../../assets/SideImage.png";
 
-const BuyerSignUp=() =>{
+const BuyerSignUp = () => {
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    <div>
-      <h1 className='text-black'>BuyerSignup</h1>
+    <>
+      <div className="w-screen flex flex-col items-center">
+        <div className="w-screen h-[10vh]"></div>
+        <div className="w-[80vw] flex justify-between">
+          <img src={img} alt="" className="w-[40rem]" />
+          <div className="auth w-[50%] flex justify-center items-center">
+            {isLogin ? <Login isLogin={isLogin} setIsLogin={setIsLogin}/> : <Register isLogin={isLogin} setIsLogin={setIsLogin}/>}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Login = ({ isLogin, setIsLogin }) => {
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const result = await axios.post("http://localhost:3000/loginbuyer", {
+        username,
+        password,
+      });
+
+      setCookies("access_token", result.data.token);
+      window.localStorage.setItem("userID", result.data.userID);
+      window.localStorage.setItem("userType", result.data.userType);
+
+      navigate("/");
+      console.log(result.data.token);
+      console.log(result.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="auth-container w-[100%] flex justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-10 w-[100%] items-center"
+      >
+        <h2 className="text-black text-center text-[2rem]">Login as a Buyer </h2>
+        <div className="form-group flex gap-2 justify-center w-[100%]">
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            className="border-b border-black p-1 w-[50%] text-black focus:outline-none focus:text-[20px]"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-group flex justify-center gap-2 w-[100%]">
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="border-b border-black p-1 w-[50%] text-black focus:outline-none focus:text-[20px]"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="w-[50%] p-2 bg-blue-400 rounded-2xl text-[20px]">
+          Login
+        </button>
+        <p className="text-black">
+          Create an Account{" "}
+          <span
+            className="text-blue-400 cursor-pointer"
+            onClick={() => {
+              setIsLogin(!isLogin);
+            }}
+          >
+            Register
+          </span>
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+const Register = ({ isLogin, setIsLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("http://localhost:3000/signupbuyer", {
+        username,
+        password,
+      });
+      alert("Registration Completed! Now login.");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <div className="auth-container w-[100%] flex justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-10 w-[100%] items-center"
+      >
+        <div className="flex flex-col gap-3">
+          <h2 className="text-black text-center text-[2rem]">
+            Create an Account
+          </h2>
+          <p className="text-black">Enter your details here</p>
+        </div>
+        <div className="form-group flex gap-2 justify-center w-[100%]">
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            className="border-b border-black p-1 w-[50%] text-black focus:outline-none focus:text-[20px]"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-group flex gap-2 justify-center w-[100%]">
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="border-b border-black p-1 w-[50%] text-black focus:outline-none focus:text-[20px]"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="p-2 bg-blue-400 rounded-2xl w-[50%]">Register</button>
+        {!isLogin && (
+          <p className="text-black">
+            Already have account?{" "}
+            <span
+              className="text-blue-400 cursor-pointer"
+              onClick={() => {
+                setIsLogin(!isLogin);
+              }}
+            >
+              LogIn
+            </span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
 
 export default BuyerSignUp;
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import img from '../../assets/SideImage.png';
+import { Link } from 'react-router-dom';
 
 const Signup=() =>{
   return (
-    <div>
-      <h1 className='text-black'>Login</h1>
-    </div>
+    <>
+      <div className="w-screen flex flex-col items-center">
+        <div className="w-screen h-[10vh]"></div>
+        <div className='w-[80%] flex justify-between'>
+        <img src={img} alt="" className='w-[40rem]'/>
+        <div className='w-[50%] flex flex-col justify-center items-center gap-10'>
+          <div className=' flex flex-col gap-4 items-center'>
+          <p className='text-black text-[30px]'>You want to <span className='font-bold'>Buy</span> Something <span className='text-red-400'>Click Here</span></p>
+          <button className='bg-blue-400 w-[50%] p-3 rounded-3xl'>
+            <Link to='/signup/buyer' className='text-white hover:text-black'>
+              Sign Up as Buyer
+            </Link>
+          </button>
+          </div>
+
+          <div className=' flex flex-col gap-4 items-center'>
+          <p className='text-black text-[30px]'>You want to <span className='font-bold'>Sell</span> Something <span className='text-red-400'>Click Here</span></p>
+          <button className='bg-blue-400 w-[50%] p-3 rounded-3xl'>
+            <Link to='/signup/seller' className='text-white hover:text-black'>
+              Sign Up as Seller
+            </Link>
+          </button>
+          </div>
+        </div>
+        </div>
+      </div>
+    </>
   )
 }
 
