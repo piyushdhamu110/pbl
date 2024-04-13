07ca06caaa4d5732df0e340c835b3b088051bc58
darkars33 <darshangarad18@gmail.com>
2024-04-13 13:09:23 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-400 bg-black'>hello</h1>
+    </>
+  )
+}
+
+export default App
